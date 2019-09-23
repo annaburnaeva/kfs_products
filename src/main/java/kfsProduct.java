ba7427f0f0e8f634669d46384e6ed4a98ee825fb
student <student@localhost>
2019-09-23 16:00:05 +0300

    Initial commit

---
+++ src/main/java/kfsProduct.java
@@ -0,0 +1,14 @@
+public class kfsProduct {
+    long id;
+    String name;
+    String image;
+    String description;
+    int price;
+    int calorific;
+    int weight;
+    double proteins;
+    double fats;
+    double carbohydrates;
+
+
+}

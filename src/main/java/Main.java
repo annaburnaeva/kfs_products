c9ab38798f68bbd68e0a0f0771166f12afc65e9d
student <student@localhost>
2019-09-24 12:30:00 +0300

    Added object description

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        kfsProduct twisterDeLuxeSpicy  = new kfsProduct();
-        kfsProduct longerBBQ = new kfsProduct();
+        kfsProduct twisterDeLuxSpicy = new kfsProduct();
+        twisterDeLuxSpicy.id = 2534;
+        twisterDeLuxSpicy.name = "Твистер Де Люкс острый";
+        twisterDeLuxSpicy.description = "Любимый Твистер теперь Де Люкс: пшеничная лепешка с сочными острыми куриными стрипсами в панировке, с горчицей, кетчупом, беконом, луком, сыром эменталер, маринованными огурчиками и свежим салатом";
+        twisterDeLuxSpicy.price = 184;
+        twisterDeLuxSpicy.calorific = 239;
+        twisterDeLuxSpicy.weight = 211;
+        twisterDeLuxSpicy.proteins = 12.2;
+        twisterDeLuxSpicy.fats = 10.6;
+        twisterDeLuxSpicy.carbohydrates = 23.7;
+
+//
+        kfsProduct cheeseburger = new kfsProduct();
+        cheeseburger.id = 2336;
+        cheeseburger.name = "Чизбургер";
+        cheeseburger.description = "Пряный горчичный соус, кетчуп, сочные стрипсы в оригинальной панировке, лук, сыр Чеддер, огурцы на пшеничной булочке с кукурузной посыпкой.";
+        cheeseburger.price = 69;
+        cheeseburger.calorific = 237;
+        cheeseburger.weight = 167;
+        cheeseburger.proteins = 13.2;
+        cheeseburger.fats = 8.6;
+        cheeseburger.carbohydrates = 26.6;
+
 
     }
 }
